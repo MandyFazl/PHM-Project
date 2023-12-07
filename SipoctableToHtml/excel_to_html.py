0a065e84433fdb01514a0cc52f5acfc56031afb9
Mandy Fazl <m.fazl.zarandi@gmail.com>
2023-12-07 23:05:37 +0000

    main

---
+++ SipoctableToHtml/excel_to_html.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def excel_to_html(input_excel, output_html):
+    # Read Excel file into a Pandas DataFrame
+    df = pd.read_excel(input_excel)
+
+    # Convert DataFrame to HTML table
+    html_table = df.to_html(index=False, classes='table table-bordered table-striped')
+
+    # Write the HTML table to an output file
+    with open(output_html, 'w') as f:
+        f.write(html_table)
+
+if __name__ == "__main__":
+    # Replace 'input_excel.xlsx' with the path to your Excel file
+    input_excel_file = 'Excel-format.xlsx'
+
+    # Replace 'output_table.html' with the desired output HTML file path
+    output_html_file = 'output_table.html'
+
+    excel_to_html(input_excel_file, output_html_file)
